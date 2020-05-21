33851dc3d8b121a98a45db0171670e6e6f81c964
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-05-21 13:11:04 +0100

    Variable defination in C

---
+++ Variable.c
@@ -0,0 +1,28 @@
+/*inculding preprocessor*/
+
+#include<stdio.h>
+
+/*main function of a programing*/
+
+int main()
+{
+
+/*Note: the syntax for varibale definition is "data_type variable_name = value; " or "data_type variable_name; " or "data_type variable_name1,variable_name2,,.....variable_namen;"*/
+
+/* A Variable Name can be of any user defined here in this program i have given a,b,c as a variable name*/
+ 
+int a= 5;
+
+char  b='a';
+
+float c=20.450;
+
+/*Accessing the variable value by using the variable name*/
+
+print("\nThe value of int a is : %d \n",a);
+
+print("\nThe value of char b is : %c \n",b);
+
+print("\nThe value of float c is : %f  \n",c);
+
+}
