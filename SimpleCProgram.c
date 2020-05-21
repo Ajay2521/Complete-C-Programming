c1efec19d09f9c5a460237ef9f7f6ab3d1812fa8
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-05-21 10:18:03 +0100

    Simple C Program

---
+++ SimpleCProgram.c
@@ -0,0 +1,18 @@
+/*the first line of the program consist of preprocessor commands which is used to inculde a pre-defined funcctions avaliable in c progarmming which is needed to built the c program*/
+
+#include <stdio.h>
+
+/*then we create create the fuction where statements and codes will be present and which will be working when the program is compiled*/
+
+/*Note: A C program must contain atleast one main () function in a entier C program*/
+  
+int main()
+{
+
+/* printf is standard per defined function which is used to print the statement which is present inside the double quotes */
+printf("Hello, World! \n");
+
+/*return will be used is return the value of a function , it was an optional one */
+
+return 0;
+}
