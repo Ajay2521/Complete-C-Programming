0ec91540c54857d7bf3a906924efa06dcbde0f26
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-05-21 11:26:45 +0100

    Program for finding Storage Size of all Data Type in C

--- DataType.c
+++ DataType.c
@@ -10,48 +10,50 @@ int main()
 
 /*sizeof is a predefined function used to find the size of a variable or data type*/
 
+/*In this program is have used '/n' in lot of places , it a special character or excape sequence used for next line , Don't worry this topic will be covered later on */
+
 /* finding storage size for int*/
 
-printf("Storage size for int : %d \n", sizeof(int)," byte");
+printf("\nStorage size for int : %d \n", sizeof(int));
 
-printf("Storage size for unsigned int : %d \n", sizeof(unsigned int)," byte");
+printf("\nStorage size for unsigned int : %d \n", sizeof(unsigned int));
 
-printf("Storage size for signed int : %d \n", sizeof(signed int)," byte");
+printf("\nStorage size for signed int : %d \n", sizeof(signed int));
 
 /* finding storage size for char*/
 
-printf("Storage size for char : %d \n", sizeof(char)," byte");
+printf("\nStorage size for char : %d \n", sizeof(char));
 
-printf("Storage size for unsigned char : %d \n", sizeof(unsigned char)," byte");
+printf("\nStorage size for unsigned char : %d \n", sizeof(unsigned char));
 
-printf("Storage size for signed char : %d \n", sizeof(signed char)," byte");
+printf("\nStorage size for signed char : %d \n", sizeof(signed char));
 
 /* finding storage size for short*/
 
-printf("Storage size for short : %d \n", sizeof(short)," byte");
+printf("\nStorage size for short : %d \n", sizeof(short));
 
-printf("Storage size for unsigned short : %d \n", sizeof(unsigned short)," byte");
+printf("\nStorage size for unsigned short : %d \n", sizeof(unsigned short));
 
-printf("Storage size for signed short : %d \n", sizeof(signed short)," byte");
+printf("\nStorage size for signed short : %d \n", sizeof(signed short));
 
 /* finding storage size for long*/
 
-printf("Storage size for long : %d \n", sizeof(long)," byte");
+printf("\nStorage size for long : %d \n", sizeof(long));
 
-printf("Storage size for unsigned long : %d \n", sizeof(unsigned long)," byte");
+printf("\nStorage size for unsigned long : %d \n", sizeof(unsigned long));
 
-printf("Storage size for signed long : %d \n", sizeof(signed long)," byte");
+printf("\nStorage size for signed long : %d \n", sizeof(signed long));
 
 /* finding storage size for float*/
 
-printf("Storage size for float : %d \n", sizeof(float)," byte");
+printf("\nStorage size for float : %d \n", sizeof(float));
 
 /* finding storage size for double*/
 
-printf("Storage size for double : %d \n", sizeof(double)," byte");
+printf("\nStorage size for double : %d \n", sizeof(double));
 
 /* finding storage size for long double*/
 
-printf("Storage size for long double : %d \n", sizeof(long double)," byte");
+printf("\nStorage size for long double : %d \n", sizeof(long double));
 
 }
