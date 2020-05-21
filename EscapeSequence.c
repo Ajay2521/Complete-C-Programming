72ee2d0db6398f48b44276935a217d26a4b02058
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-05-21 13:38:21 +0100

    Defined some majorly used Escape Sequence in C

---
+++ EscapeSequence.c
@@ -0,0 +1,35 @@
+/*inculding preprocessor*/
+
+#include <stdio.h>
+
+/*main function of a programing*/
+
+int main()
+{
+
+/*Here Manjor Escape Sequence are explained here*/
+
+printf("\nBalck Slash will be printed : \\ \n");
+
+print("\nSingle quotes will be printed : \' \n");
+
+print("\nDouble quotes will be printed : \'' \n");
+
+print("\nQuestion mark will be printed : \? \n");
+
+print("\nAlert or bell sound will be produced :  \a \n");
+
+print("\nBackSpace will be printed : \b successfully \n");
+
+print("\nForm Feed will be printed : \f successfully \n");
+
+print("\nNew line will be printed : \nsuccessfully\n ");
+
+print("\nHorizontal tab will be printed : \tsuccessfully\n ");
+
+print("\nVertical tab will be printed : \vsuccessfully\n ");
+}
+
+
+
+ 
