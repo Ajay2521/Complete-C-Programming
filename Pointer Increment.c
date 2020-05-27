d4666a894f633821c3b83dd5d93b12b791e40033
MAAYON_AJ <MAAYON@MAAYON_AJ>
2020-05-27 12:26:18 +0100

    In this Pointer Arithematic concept of C Program has been explained

---
+++ Pointer Increment.c
@@ -0,0 +1,42 @@
+/*In this program we will be seeing about POINTERS in C Program*/
+ 
+/*Pointer is a type of user defined variable which is used to store the ADDRESS of other variable*/
+
+/*pointers are used for dynamic memory allocation ,which is used when we create a dynamic program*/
+
+/*In pointer we can able to do certain artimatic operation like increment,decrement...etc,,*/
+
+/*In this program lets see about incrementing a pointer in aprogram*/
+
+/*including preprocessor in the program*/
+
+#include <stdio.h>
+
+/*creating a main() function of the program*/
+
+int main()
+
+{
+
+int arr[] = {10, 20, 30,40};
+
+int i, *ptr;
+
+/* let us have array address in pointer */
+
+ptr = arr;
+
+for ( i = 0; i < 4; i++)
+
+{
+	
+	printf("\nAddress of arr[%d] = %x\n", i, ptr );
+
+	printf("\nValue of arr[%d] = %d\n", i, *ptr );
+	
+	/* move to the next location */
+
+	ptr++;
+}
+
+}
